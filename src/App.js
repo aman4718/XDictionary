1603ff0960c246b297b5ef60f0c64085397cea37
aman4718 <bholukumar4718@gmail.com>
2025-01-27 00:34:50 +0530

    xdict

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import XDictionary from './components/XDictionary';
+
+function App() {
+  return (
+    <div className="App">
+      <XDictionary/>
+    </div>
+  );
+}
+
+export default App;
